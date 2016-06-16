6956ed9d54e95ccd7c0b0dca4a116746543e28ab
Dennis <dennis_pham@hotmail.com>
2016-06-16 12:40:36 -0700

    class 4

---
+++ Dennis/class_1/Calculator/Calculator/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Calculator
+//
+//  Created by Dennis Pham on 2016-06-16.
+//  Copyright © 2016 Dennis Pham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ Dennis/class_4/MovingManyThings/MovingManyThings/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MovingManyThings
+//
+//  Created by Dennis Pham on 2016-06-16.
+//  Copyright © 2016 Dennis Pham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
