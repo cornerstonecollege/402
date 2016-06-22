fa49895b1ac61eb1313452fee937d93fd7eb77de
Dennis <dennis_pham@hotmail.com>
2016-06-22 13:06:58 -0700

    class 8

---
+++ Dennis/class_8/TableView/TableView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TableView
+//
+//  Created by Dennis Pham on 2016-06-22.
+//  Copyright © 2016 Dennis Pham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Dennis/class_8/TableView/TableView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TableView
+//
+//  Created by Dennis Pham on 2016-06-22.
+//  Copyright © 2016 Dennis Pham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
