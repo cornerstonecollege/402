f1b47f95609c300aa3d9fe8d7f18810731b42612
Digby Andrews <digbysa@gmail.com>
2016-07-13 13:49:36 -0700

    class_17

--- Digby/class_17/MapsLocation/MapsLocation/ViewController.swift
+++ Digby/class_17/MapsLocation/MapsLocation/ViewController.swift
@@ -7,17 +7,55 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+class ViewController: UIViewController, CLLocationManagerDelegate
+{
+    @IBOutlet weak var mapView: MKMapView!
+    let locationHandler = CLLocationManager()
+    
+    override func viewDidLoad()
+    {
+        self.createLocation()
     }
 
-    override func didReceiveMemoryWarning() {
+    func createLocation ()
+    {
+        super.viewDidLoad()
+        if CLLocationManager.authorizationStatus() !=
+            CLAuthorizationStatus.AuthorizedWhenInUse
+        {
+            self.locationHandler.requestWhenInUseAuthorization()
+        }
+        
+        if CLLocationManager.authorizationStatus() ==
+            CLAuthorizationStatus.AuthorizedWhenInUse
+        {
+            self.locationHandler.delegate = self
+            self.locationHandler.desiredAccuracy = kCLLocationAccuracyBest
+            self.locationHandler.distanceFilter = 10
+            self.locationHandler.startUpdatingLocation()
+        }
+        else
+        {
+            print("The user didn't allow")
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
+    {
+        if let location = locations.first
+        {
+            let span = MKCoordinateSpanMake(1, 1)
+            
+            let region = MKCoordinateRegionMake(location.coordinate, span)
+            
+            self.mapView.setRegion(region, animated: true)
+        }
+    }
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
