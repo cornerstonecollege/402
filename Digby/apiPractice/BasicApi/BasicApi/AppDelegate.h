c10fba26862cdb5ea45c66a19a6a357300700906
Digby Andrews <digbysa@gmail.com>
2016-08-10 10:39:17 -0700

    Started learning php

---
+++ Digby/apiPractice/BasicApi/BasicApi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BasicApi
+//
+//  Created by Digby Andrews on 2016-08-09.
+//  Copyright © 2016 Digby Andrews. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Digby/apiPractice/BasicApi/BasicApi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BasicApi
+//
+//  Created by Digby Andrews on 2016-08-09.
+//  Copyright © 2016 Digby Andrews. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
