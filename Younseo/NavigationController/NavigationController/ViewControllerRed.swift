ae46c72a1a9823b4a3a65f7bbf0f7c11199fe6ef
imconnie <younseoh@gmail.com>
2016-10-18 15:30:19 -0700

    Navigation Controller

---
+++ Younseo/NavigationController/NavigationController/ViewControllerRed.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NavigationController
+//
+//  Created by hoconey on 2016/10/18.
+//  Copyright © 2016年 younseo. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerRed: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+     print("RED")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
